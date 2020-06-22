3adf60cbc1cce94775d69b52ce8ab3dbf1f1eeab
Corvus <wyatta.hancock@gmail.com>
2020-06-22 11:39:03 -0600

    added proper time, now I just need to figure out how to make it live

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
 //Document.ready will make sure that the entire javascript is prepared before running
 $(document).ready(function(){
     //call upon the moment.js to display the current date
-    var currentDate = moment().format("MMMM Do YYYY");
     var dayOfWeek = moment().format("dddd");
+    var currentDate = moment().format("MMMM Do YYYY");
     var currentInfo = "The Current Date: " + dayOfWeek + ", " + currentDate;
     $("#currentDay").text(currentInfo);
+
+    //call upon the moment.js to display the current time
+    var nowHour = moment().format("H HH");
+    var nowMinute= moment().format("m mm");
+    var nowSec= moment().format("s ss")
+    var nowTime= nowHour + ":" +nowMinute + ":" + nowSec;
+    $("#currentTime").text(nowTime);
 
 })
 
