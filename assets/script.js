68053f86114dbe6298a41e86318ef09e5401383f
Corvus <wyatta.hancock@gmail.com>
2020-06-22 14:05:15 -0600

    added the proper data tribute so that the for loop is able to detect what time the timeblock is. Got some help from instructional assistant

--- assets/script.js
+++ assets/script.js
@@ -7,27 +7,30 @@ $(document).ready(function(){
     $("#currentDay").text(currentInfo);
 
     //call upon the moment.js to display the current time
-    // function updateTime(){
-    //     var nowHour = moment().format("HH");
-    //     var nowMinute= moment().format("mm");
-    //     var nowSec= moment().format("ss")
-    //     var nowTime= nowHour+ ":" + nowMinute + ":" + nowSec;
-    //     $("#currentTime").text(nowTime);
+    function updateTime(){
+        var nowHour1 = moment().format("HH");
+        var nowMinute= moment().format("mm");
+        var nowSec= moment().format("ss")
+        var nowTime= nowHour1+ ":" + nowMinute + ":" + nowSec;
+        $("#currentTime").text(nowTime);
         
-    // }
-    //     //Used setInterval to update the timer every second
-    //     setInterval(updateTime, 1000);
+    }
+        //Used setInterval to update the timer every second
+        setInterval(updateTime, 1000);
 
     var nowHour = moment().hours();
     for (i=0; i < 9; i++){
-        if ($(".work-hr").eq(i).data("work-hr") < nowHour){
+        console.log($(".work-hr").eq(i).data("hour"));
+        debugger
+        if ($(".work-hr").eq(i).data("hour") < nowHour){
             ($(".textarea").eq(i).addClass("past"));
         }
-        else if ($(".work-hr").eq(i).data("work-hr") === nowHour){
+        else if ($(".work-hr").eq(i).data("hour") === nowHour){
             ($(".textarea").eq(i).addClass("present"));
         }
         else {($(".textarea").eq(i).addClass("future"));
     }}
+        setInterval(nowHour, 1000);
 
 
 
