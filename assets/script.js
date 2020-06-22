1201767701e6963cc23c5fb85d8f91bb4c2f96a7
Corvus <wyatta.hancock@gmail.com>
2020-06-22 12:28:21 -0600

    trying to color code the calendar, having trouble doing so

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,33 @@ $(document).ready(function(){
     $("#currentDay").text(currentInfo);
 
     //call upon the moment.js to display the current time
-    function updateTime(){
-        var nowHour = moment().format("HH");
-        var nowMinute= moment().format("mm");
-        var nowSec= moment().format("ss")
-        var nowTime= nowHour+ ":" + nowMinute + ":" + nowSec;
-        $("#currentTime").text(nowTime);
+    // function updateTime(){
+    //     var nowHour = moment().format("HH");
+    //     var nowMinute= moment().format("mm");
+    //     var nowSec= moment().format("ss")
+    //     var nowTime= nowHour+ ":" + nowMinute + ":" + nowSec;
+    //     $("#currentTime").text(nowTime);
+        
+    // }
+    //     //Used setInterval to update the timer every second
+    //     setInterval(updateTime, 1000);
+
+    var nowHour = moment().hours();
+    for (i=0; i < 9; i++){
+        if ($(".hour").eq(i).data("hour") < nowHour){
+            ($(textarea).eq(i).addClass("past"));
+        }
+
+
+
     }
-        //Used setInterval to update the timer every second
-        setInterval(updateTime, 1000);
+
+
+
+
 })
+
+
 
 
 
